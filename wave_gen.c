cc5606a22357387f2f6994094b3aa1d4388c58d2
AmanoKokoro <55781271+AmanoKokoro@users.noreply.github.com>
2020-08-12 11:55:16 +0900

    Debug with VScode

---
+++ wave_gen.c
@@ -0,0 +1,56 @@
+//VisualStudio�΍�i���w�萔�̎g�p������
+//#define _USE_MATH_DEFINES
+
+#include"wave.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+#define DATASIZE 48000
+
+int main(int argc, char *argv[])
+{
+	unsigned long length,loop;
+	signed short wavedt[DATASIZE];
+	Sound snd;
+	
+	unsigned int freq = 880;
+	signed short amplitude = 10000;
+	
+	if(argc != 2){
+		printf("Usage: program <outputfile>\n");
+		return 1;
+	}
+
+	
+
+	snd.channelnum = 1;
+	snd.samplingrate = 48000;
+	snd.bit_per_sample = 16;
+	snd.datanum = DATASIZE;
+	snd.monaural16 = wavedt;
+	
+	printf("FILE INFO\n");
+	printf("channelnum = %d\n",snd.channelnum);
+	printf("samplingrate = %d\n",snd.samplingrate);
+	printf("bit_per_sample = %d\n",snd.bit_per_sample);
+	printf("datanum = %d\n",snd.datanum);
+	
+	printf("\nWAVE INFO\n");
+	printf("freq = %d\n",freq);
+	printf("amplitude = %d\n",amplitude);
+	
+	
+	for(loop=0;loop<DATASIZE;loop++){
+		wavedt[loop] = amplitude * sin(2*M_PI*freq*((double)loop/snd.samplingrate));
+	}
+	
+	if(Write_Wave(argv[1], &snd)){
+		Free_Sound(&snd);
+		return 1;
+	}
+
+	Free_Sound(&snd);
+
+	return 0;
+}
