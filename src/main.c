bb6758175a050060beee902e800c1afbddb68ee0
AmanoKokoro <55781271+AmanoKokoro@users.noreply.github.com>
2020-08-19 22:52:30 +0900

    edit main.c

--- src/main.c
+++ src/main.c
@@ -1,31 +1,55 @@
-#include "dtmf.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
-void help();
-
-int main(int argv, char *argc[])
+int main(int argc, char *argv[])
 {
-	Sound dtmf;								//Sound型構造体
-	signed char wavedt[DATACHANCSIZE];		//Sound型dtmfに渡す15桁のDTMF信号のデータが入る配列
-	signed char datadt[DIGIT][DATASIZE];	//[n桁目の数字][]
+	Sound dtmf;						   //Sound型構造体
+	signed char wavedt[DATACHANCSIZE]; //Sound型dtmfに渡す15桁のDTMF信号のデータが入る配列
+	signed char datadt[DATASIZE];	   //[n桁目の数字][]
 	signed char pausedt[PAUSESIZE];
 
-	if (argv != 3 || strstr(argc[2], ".wav") || strstr(argc[1], "-h"))
+	if (argc != 3)
 	{
 		help();
 	}
 
 	pausegen(pausedt);
 
+	for (char loop = 0; loop < DIGIT; loop++)
+	{
+		dtmfgen(argv[1][loop], datadt);
+		memcat(wavedt, datadt, DATASIZE);
+		memcat(wavedt, pausedt, PAUSESIZE);
+	}
+
+	dtmf = dtmfwaveset(wavedt);
+
+	if(Write_Wave(argv[2], &dtmf))
+	{
+		Free_Sound(&dtmf);
+		return 1;
+	}
+
+	Free_Sound(&dtmf);
 	return 0;
 }
 
+//Help
+//Help表示関数
 void help()
 {
 	printf("Usage\n");
 	printf("DTMF_gen [NUMBER] [OUTPUTFILE]\n\n");
-	printf("OPTIONS\n");
-	printf("-h\tPrint this help.\n");
+}
+
+//memcat
+//配列結合関数dstにsrcを追加する関数
+//dstは書き換えられる
+void memcat(char dst[], char src[], unsigned int limit)
+{
+	static unsigned long dstloop = 0;
+	unsigned long srcloop;
+	for (srcloop = 0; srcloop < limit || dstloop < DATACHANCSIZE; dstloop++, srcloop++)
+	{
+		dst[dstloop] = src[srcloop];
+	}
 }
---
+++ src/main.h
@@ -0,0 +1,12 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "dtmf.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void help();
+void memcat(char *, char *, unsigned int);
+
+#endif //__MAIN_H
